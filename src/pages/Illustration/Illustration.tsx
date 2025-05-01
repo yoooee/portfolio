a2d8ca3d1b2865abc4fbd4f50e10ea1ec0350613
Joe Staroba <joe@staroba.com>
2025-05-01 11:52:52 -0500

    Add illustration samples

--- src/pages/Illustration/Illustration.tsx
+++ src/pages/Illustration/Illustration.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import type { portfolioItem } from '../../interfaces';
+import Card from '../../components/Card/Card';
+import CardList from '../../components/CardList/CardList';
 
 function Illustration() {
+  const portfolioItemList: portfolioItem [] = [
+    {image: 'arnold.jpg'}, 
+    {image: 'spawn.jpg'}, 
+    {image: 'super-heroine.jpg'}, 
+    {image: 'wolverine.jpg'}, 
+  ];
+
   return (
-    <div> Illustration </div>
+    <div className="design-cards">
+      <CardList>
+        {portfolioItemList.map((portfolioItem, key) => <Card key={key} thumbnail={portfolioItem.image}> </Card>)}
+      </CardList>
+
+    </div>
   );
 }
 
