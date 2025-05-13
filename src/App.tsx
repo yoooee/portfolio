3e5aef1040c5c794f658f5776482cacd9fb242d2
Joe Staroba <joe@staroba.com>
2025-05-13 11:55:41 -0500

    Add home page component and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { NavLink, Routes, Route} from 'react-router';
 import Navigation from './components/Navigation/Navigation';
 import LogoMobile from './assets/logo-mobile.svg';
 import LogoDesktop from './assets/logo-desktop.svg';
-import {Routes, Route} from 'react-router';
+import Home from './pages/Home/Home';
 import Design from './pages/Design/Design';
 import Development from './pages/Development/Development';
 import Illustration from './pages/Illustration/Illustration';
@@ -13,8 +14,10 @@ function App() {
   return (
       <div className="App">
         <header>
-          <img className="logo-mobile" src={LogoMobile} alt="The Portfolio of Josef Staroba" />
-          <img className="logo-desktop" src={LogoDesktop} alt="The Portfolio of Josef Staroba" />
+          <NavLink to="/">
+            <img className="logo-mobile" src={LogoMobile} alt="The Portfolio of Josef Staroba" />
+            <img className="logo-desktop" src={LogoDesktop} alt="The Portfolio of Josef Staroba" />
+          </NavLink>
         </header>
         <nav><Navigation /></nav> 
         <div className="portfolio">
@@ -23,6 +26,7 @@ function App() {
             <Route path="/development" element={<Development />} />
             <Route path="/illustration" element={<Illustration />} />
             <Route path="/photography" element={<Photography />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </div>
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import './home.scss';
+import Photo from '../../assets/photo.jpeg';
+
+function Home() {
+  return (
+    <div className="home">
+      <article>
+        <p>My name is Josef Staroba and I am a software designer/developer with over 25 years of professional experience. </p>
+        <p>On this site, you will find a small sampling of my work over the span of my career.</p>
+      </article>
+      <img src={Photo} alt="Josef Staroba" />
+    </div>
+  );
+}
+
+export default Home;
